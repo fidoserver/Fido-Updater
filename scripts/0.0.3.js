cc50a013a73107540da08e977ff2298a95775d62
rjsteinert <rj@rjsteinert.com>
2014-05-15 16:21:26 -0400

    0.0.3

--- scripts/0.0.3.js
+++ scripts/0.0.3.js
@@ -3,8 +3,8 @@ var cmd = ''
 function puts(error, stdout, stderr) { sys.puts(stdout) }
 
 module.exports = function(callback) {
-  console.log('running script 0.0.2.js')
-  cmd='cd /root/Fido; git pull origin master; git fetch --tags; git checkout master; git checkout 0.0.2;'
+  console.log('running script 0.0.3.js')
+  cmd='cd /root/Fido; git pull origin master; git fetch --tags; git checkout master; git checkout 0.0.3;'
   exec(cmd, function() {
     callback()
   })
