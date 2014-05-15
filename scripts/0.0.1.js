e87bf09621e9111847dbc4a6ed3e7f3062fe58c7
rjsteinert <rj@rjsteinert.com>
2014-05-15 15:43:05 -0400

    Update to 0.0.1

--- scripts/0.0.1.js
+++ scripts/0.0.1.js
@@ -3,8 +3,8 @@ var cmd = ''
 function puts(error, stdout, stderr) { sys.puts(stdout) }
 
 module.exports = function(callback) {
-  console.log('running test 2')
-  cmd='touch /root/test2.txt'
+  console.log('running script 4.js')
+  cmd='cd /root/Fido; git pull; git checkout 0.0.1;'
   exec(cmd, function() {
     callback()
   })
