6a8ee063b1e13d467da3eb5b063077e2e62604e2
rjsteinert <rj@rjsteinert.com>
2014-05-15 15:46:53 -0400

    Correct comment

--- scripts/0.0.1.js
+++ scripts/0.0.1.js
@@ -3,7 +3,7 @@ var cmd = ''
 function puts(error, stdout, stderr) { sys.puts(stdout) }
 
 module.exports = function(callback) {
-  console.log('running script 4.js')
+  console.log('running script 0.0.1.js')
   cmd='cd /root/Fido; git pull; git checkout 0.0.1;'
   exec(cmd, function() {
     callback()
