c47dd9889c53b44236845a6bab977111bdd2e6b1
R.J. Steinert <rj@rjsteinert.com>
2014-07-18 05:56:59 +0000

    Missing semicolon on 0.0.4 update script

--- scripts/0.0.4.js
+++ scripts/0.0.4.js
@@ -8,7 +8,7 @@ module.exports = function(callback) {
   cmd += 'git clone https://github.com/padelt/temper-python.git; '
   cmd += 'cd temper-python; '
   cmd += 'sudo apt-get install python-usb; '
-  cmd += 'sudo python setup.py install '
+  cmd += 'sudo python setup.py install; '
   cmd += 'cd /root/Fido; git pull origin master; git fetch --tags; git checkout master; git checkout 0.0.4;'
   exec(cmd, function() {
     callback()
