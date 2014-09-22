161de124dbaa2945cd6fef1dfee30f3dadaeac34
rjsteinert <rj@rjsteinert.com>
2014-09-22 18:56:53 -0400

    Spun out the 0.0.5 script to a shell script and made the original js update script compliant with this installer.

--- scripts/0.0.5.js
+++ scripts/0.0.5.js
@@ -1,121 +1,14 @@
-cd /root;
-git clone https://github.com/DexterInd/GrovePi;
-cd GrovePi/Script;
+var exec = require('child_process').exec;
+var cmd = ''
+function puts(error, stdout, stderr) { sys.puts(stdout) }
 
-# START GROVEPI SCRIPT
-#
-#
-#
-#
-#
-#
-#
-
-echo "  _____            _                                ";
-echo " |  __ \          | |                               ";
-echo " | |  | | _____  _| |_ ___ _ __                     ";
-echo " | |  | |/ _ \ \/ / __/ _ \ '__|                    ";
-echo " | |__| |  __/>  <| ||  __/ |                       ";
-echo " |_____/ \___/_/\_\\__\___|_| _        _            ";
-echo " |_   _|         | |         | |      (_)           ";
-echo "   | |  _ __   __| |_   _ ___| |_ _ __ _  ___  ___  ";
-echo "   | | | '_ \ / _\` | | | / __| __| '__| |/ _ \/ __|";
-echo "  _| |_| | | | (_| | |_| \__ \ |_| |  | |  __/\__ \ ";
-echo " |_____|_| |_|\__,_|\__,_|___/\__|_|  |_|\___||___/ ";
-echo "                                                    ";
-echo "                                                    ";
-echo " "
-echo " "
-
-echo "Check for internet connectivity..."
-echo "=================================="
-wget -q --tries=2 --timeout=20 http://google.com
-if [[ $? -eq 0 ]];then
-	echo "Connected"
-else
-	echo "Unable to Connect, try again !!!"
-	exit 0
-fi
-
-echo " "
-echo "Installing Dependencies"
-echo "======================="
-sudo apt-get install python-pip git libi2c-dev python-serial python-rpi.gpio i2c-tools python-smbus arduino minicom
-echo "Dependencies installed"
-
-git clone git://git.drogon.net/wiringPi
-cd wiringPi
-./build
-echo "wiringPi Installed"
-
-echo " "
-echo "Removing blacklist from /etc/modprobe.d/raspi-blacklist.conf . . ."
-echo "=================================================================="
-if grep -q "#blacklist i2c-bcm2708" /etc/modprobe.d/raspi-blacklist.conf; then
-	echo "I2C already removed from blacklist"
-else
-	sudo sed -i -e 's/blacklist i2c-bcm2708/#blacklist i2c-bcm2708/g' /etc/modprobe.d/raspi-blacklist.conf
-	echo "I2C removed from blacklist"
-fi
-if grep -q "#blacklist spi-bcm2708" /etc/modprobe.d/raspi-blacklist.conf; then
-	echo "SPI already removed from blacklist"
-else
-	sudo sed -i -e 's/blacklist spi-bcm2708/#blacklist spi-bcm2708/g' /etc/modprobe.d/raspi-blacklist.conf
-	echo "SPI removed from blacklist"
-fi
-
-#Adding in /etc/modules
-echo " "
-echo "Adding I2C-dev and SPI-dev in /etc/modules . . ."
-echo "================================================"
-if grep -q "i2c-dev" /etc/modules; then
-	echo "I2C-dev already there"
-else
-	echo i2c-dev >> /etc/modules
-	echo "I2C-dev added"
-fi
-if grep -q "i2c-bcm2708" /etc/modules; then
-	echo "i2c-bcm2708 already there"
-else
-	echo i2c-bcm2708 >> /etc/modules
-	echo "i2c-bcm2708 added"
-fi
-if grep -q "spi-dev" /etc/modules; then
-	echo "spi-dev already there"
-else
-	echo spi-dev >> /etc/modules
-	echo "spi-dev added"
-fi
-
-#Adding ARDUINO setup files
-echo " "
-echo "Making changes to Arduino . . ."
-echo "==============================="
-cd /tmp
-wget http://project-downloads.drogon.net/gertboard/avrdude_5.10-4_armhf.deb
-sudo dpkg -i avrdude_5.10-4_armhf.deb
-sudo chmod 4755 /usr/bin/avrdude
-
-cd /tmp
-wget http://project-downloads.drogon.net/gertboard/setup.sh
-chmod +x setup.sh
-sudo ./setup.sh
-
-#
-#
-#
-#
-#
-#
-#
-#
-# END GROVEPI SCRIPT
-
-cd /root/GrovePi/Software/Python/;
-./setup.py install;
-cd /root/GrovePi/Firmware;
-avrdude -c gpio -p m328p -U flash:w:grove_pi_firmware.hex
-cd /root/Fido; 
-git fetch; 
-git checkout origin/feature/grove_dht;
-reboot;
+module.exports = function(callback) {
+  console.log('running script 0.0.5.js')
+  cmd += 'cd /root/Fido-Updater/Scripts; '
+  cmd += 'chmod +x 0.0.5.sh; '
+  cmd += './0.0.5.sh; '
+  cmd += 'cd /root/Fido; git pull origin master; git fetch --tags; git checkout master; git checkout 0.0.5;'
+  exec(cmd, function() {
+    callback()
+  })
+}
