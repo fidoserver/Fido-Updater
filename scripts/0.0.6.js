f242a790fc0a9f8c697d30946758ed208f4f64a5
R.J. Steinert <rj@rjsteinert.com>
2014-09-23 01:16:49 +0000

    Fix 0.0.6

--- scripts/0.0.6.js
+++ scripts/0.0.6.js
@@ -1,11 +1,13 @@
 var exec = require('child_process').exec;
 var cmd = ''
-function puts(error, stdout, stderr) { sys.puts(stdout) }
 
 module.exports = function(callback) {
   console.log('running script 0.0.6.js')
-  cmd += 'cd /root/Fido; git pull origin master; git fetch --tags; git checkout master; git checkout 0.0.6;'
-  exec(cmd, function() {
+  cmd += 'cd /root/Fido;'
+  cmd += 'git remote add fidoserver https://github.com/fidoserver/Fido.git;'
+  cmd += 'cd /root/Fido; git fetch fidoserver; git fetch fidoserver --tags; git checkout fidoserver/master; git checkout 0.0.6;'
+  exec(cmd, function(error, stdout, stderr) {
+    console.log(error + " :::: " + stdout + " :::: " + stderr) 
     callback()
   })
 }
