22bcb4cbc11e4a7733ebf873296801a8994a1e8b
R.J. Steinert <rj@rjsteinert.com>
2014-09-24 19:11:24 -0400

    Update 0.0.4.js
    better logging for 0.0.4

--- scripts/0.0.4.js
+++ scripts/0.0.4.js
@@ -1,6 +1,5 @@
 var exec = require('child_process').exec;
 var cmd = ''
-function puts(error, stdout, stderr) { sys.puts(stdout) }
 
 module.exports = function(callback) {
   console.log('running script 0.0.4.js')
@@ -10,7 +9,10 @@ module.exports = function(callback) {
   cmd += 'sudo apt-get install python-usb; '
   cmd += 'sudo python setup.py install; '
   cmd += 'cd /root/Fido; git pull origin master; git fetch --tags; git checkout master; git checkout 0.0.4;'
-  exec(cmd, function() {
+  exec(cmd, function(error, stdout, stderr) {
+    console.log(error)
+    console.log(stdout)
+    console.log(stderr)
     callback()
   })
 }
